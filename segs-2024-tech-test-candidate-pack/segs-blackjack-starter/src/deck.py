d3c7fd4165b9e5d6602b9c91472f234591515b95
RegimeLeader <aamiralshaer@gmail.com>
2025-01-06 13:47:09 +0000

    Added deck class

--- segs-2024-tech-test-candidate-pack/segs-blackjack-starter/src/deck.py
+++ segs-2024-tech-test-candidate-pack/segs-blackjack-starter/src/deck.py
@@ -1,3 +1,20 @@
+import random
+
 class Deck:
     def __init__(self):
-        self.cards = []
+        self.create_deck()
+    
+    def create_deck(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+        cards = [(s[0], r) for s, r in zip(suits, ranks)]
+        return cards
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+    
+    def draw_card(self):
+        if not self.cards:
+            raise ValueError("No cards left in the deck")
+        return self.cards.pop()
